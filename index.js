39a6d42c259eaed647f240fe61528dcce2924ee6
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-01-23 09:48:29 +0100

    algo valide pass

---
+++ index.js
@@ -0,0 +1,15 @@
+const isValidPassword = (password, username) =>{
+    
+    if(password.length > 7){
+        console.log("Must be 8 caractere")
+    }
+    if(!/\s/.test(password)){
+        console.log("No space blank")
+    }
+    if(!password.includes(username)){
+        console.log("username is not contain in pass")
+    }
+
+}
+
+console.log(isValidPassword("12345paul678", "paul"))
