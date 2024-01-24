c77af58e966e1513fc9362a1459c3fc21c62d27c
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-01-24 11:57:06 +0100

    repetion LTM

--- leetcode1071.js
+++ leetcode1071.js
@@ -1,11 +1,11 @@
 function gcdOfStrings(s, t) {
     // Fonction pour vérifier si str1 est divisible par str2
-    function isDivisible(str1, str2) { //str1 = ABAB, str2 = ABAB  || str1 = ABABAB, str2 = ABAB
+    function isDivisible(str1, str2) { // 1°str1 = ABAB, str2 = ABAB  || str1 = ABABAB, str2 = ABAB :: 2°str1 : ABAB, str2 = ABA || str1 = ABABAB, str2 = ABA :: 3° str1 = ABAB, str2 = AB || str1 = ABABAB, str2 = AB 
         if (str1.length % str2.length !== 0) {
             return false;
         }
         const repetition = str1.length / str2.length;
-        return str2.repeat(repetition) === str1; //ABAB = true || false
+        return str2.repeat(repetition) === str1; //1°ABAB = true || false :: 2° false || ABA ABA ABA === ABABAB true :: 3° AB AB AB = true || AB AB AB = true
     }
 
     // Identifier la plus petite et la plus grande chaîne
@@ -13,7 +13,7 @@ function gcdOfStrings(s, t) {
 
     // Itérer sur les sous-chaînes de la plus petite chaîne
     for (let i = smaller.length; i > 0; i--) {
-        const candidate = smaller.substring(0, i);  // ABAB
+        const candidate = smaller.substring(0, i);  // 1°ABAB || 2°ABA || 3°AB
         if (isDivisible(smaller, candidate) && isDivisible(larger, candidate)) {
             return candidate;
         }
