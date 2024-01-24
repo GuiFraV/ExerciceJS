815a753df9d74b7069a3f3f304947ca5a0bf851f
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-01-24 11:38:59 +0100

    repetion LTM

--- repetitions.js
+++ repetitions.js
@@ -85,3 +85,59 @@ const gdc = (s, t) => {
     return '';
 
 }
+
+
+
+const gdcString = (s, t) => {
+
+
+    const divi = (str1, str2) => {
+
+        if(str1.length % str2.length !== 0){
+            return false;
+        }
+        const repetion = str1.length / str2.length;
+        return str2.repeat(repetion) === str1;
+    }
+
+    const [smaller, larger] = s.length < t.length ? [s, t] : [t, s];
+
+    for(let i = smaller.length; i > 0; i++){
+
+        const candidate = smaller.substring(0, i);
+        if(divi(smaller, candidate) && divi(larger, candidate)){
+            return candidate;
+        }
+
+        return candidate;
+    }
+
+    return ""
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
