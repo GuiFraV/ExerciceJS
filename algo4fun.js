078d3d8130c967417d36f7f36525449c6a7b7dd5
GuiFraV <username@users.noreply.github.com>
2024-06-21 18:11:45 +0200

    binarysearch 2/10 exercice

--- algo4fun.js
+++ algo4fun.js
@@ -24,4 +24,33 @@ const binary_search = (arr, target) => {
   return -1;
 };
 
-console.log(binary_search(arr, target));
+// console.log(binary_search(arr, target));
+
+const arr1 = [1, 2, 2, 2, 3, 4, 5];
+const target1 = 2;
+
+const find_first_occurence = (arr1, target1) => {
+  let left = 0;
+  let right = arr1.length - 1;
+  let result = -1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (arr1[middle] === target1) {
+      result = middle;
+    }
+
+    if (arr1[middle] < target1) {
+      left = middle + 1;
+    }
+
+    if (arr1[middle] > target1) {
+      right = middle - 1;
+    }
+  }
+
+  return result;
+};
+
+console.log(find_first_occurence(arr1, target1));
