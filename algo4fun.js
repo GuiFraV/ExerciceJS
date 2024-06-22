ee92cfa2a2620e2b75acf6d0c1dfeff212a2c167
GuiFraV <username@users.noreply.github.com>
2024-06-22 14:18:19 +0200

    fun

--- algo4fun.js
+++ algo4fun.js
@@ -53,4 +53,33 @@ const find_first_occurence = (arr1, target1) => {
   return result;
 };
 
-console.log(find_first_occurence(arr1, target1));
+// console.log(find_first_occurence(arr1, target1));
+
+const binarySearch = (arr, target) => {
+  let left = 0;
+  let right = arr.length - 1;
+  let result = -1;
+
+  while (left <= right) {
+    let middle = Math.floor((left + right) / 2);
+
+    if (arr[middle] === target) {
+      return middle;
+    }
+
+    if (arr[middle] > target) {
+      right = middle - 1;
+    }
+
+    if (arr[middle] < target) {
+      left = middle + 1;
+    }
+  }
+
+  return result;
+};
+
+console.log(binarySearch([1, 2, 3, 4, 5], 3)); // Output: 2
+console.log(binarySearch([1, 2, 3, 4, 5], 2)); // Output: 1
+console.log(binarySearch([1, 2, 3, 4, 5], 4)); // Output: -3
+console.log(binarySearch([1, 2, 3, 4, 5], 6)); // Output: -1
