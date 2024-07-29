ab1478662c6ebb93ee1eb265b425749017c69cdb
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-07-29 19:10:21 +0200

    Refacto js code into ts

---
+++ refactoEasy.ts
@@ -0,0 +1,19 @@
+// JavaScript code
+// function add(a, b) {
+//   return a + b;
+// }
+
+// const num1 = 5;
+// const num2 = 10;
+
+// const result = add(num1, num2);
+// console.log("Result:", result);
+
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+const num1: number = 5;
+const num2: number = 10;
+
+const result: number = add(num1, num2);
