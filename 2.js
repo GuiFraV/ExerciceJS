a7ed45fd821c46278c9b04e02721ea4ec4c99fc8
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-19 11:17:18 +0200

    Correct

--- 2.js
+++ 2.js
@@ -49,5 +49,34 @@ const jumpGame = (nums) => {
   return minJump == 0;
 };
 
-console.log(jumpGame([2, 3, 1, 1, 4]));
-console.log(jumpGame([3, 2, 1, 0, 4]));
+// console.log(jumpGame([2, 3, 1, 1, 4]));
+// console.log(jumpGame([3, 2, 1, 0, 4]));
+
+// Example 1:
+// Input: nums = [2,3,1,1,4]
+// Output: 2
+// Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+// Example 2:
+// Input: nums = [2,3,0,1,4]
+// Output: 2
+
+const jumpGame2 = (nums) => {
+  let nextMax = 0;
+  let currentMax = 0;
+  let jump = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    nextMax = Math.max(nextMax, nums[i] + i);
+
+    if (i === currentMax) {
+      jump++;
+      currentMax = nextMax;
+    }
+
+    if (currentMax >= nums.length - 1) {
+      return jumps;
+    }
+  }
+
+  return jumps;
+};
