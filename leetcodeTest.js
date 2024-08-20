c99d0d7f886975dd2156d4ce196937df899b5cd9
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-20 14:49:52 +0200

    Done

--- leetcodeTest.js
+++ leetcodeTest.js
@@ -1,9 +1,38 @@
-/**
- * @param {string} s
- * @return {number}
- */
+// /**
+//  * @param {string} s
+//  * @return {number}
+//  */
+// var romanToInt = function (s) {
+//   const romanInteger = {
+//     I: 1,
+//     V: 5,
+//     X: 10,
+//     L: 50,
+//     C: 100,
+//     D: 500,
+//     M: 1000,
+//   };
+
+//   let result = 0;
+//   let prevValue = 0;
+
+//   for (let i = s.length - 1; i >= 0; i--) {
+//     let currentValue = romanInteger[s[i]];
+
+//     if (currentValue >= prevValue) {
+//       result += currentValue;
+//     } else {
+//       result -= currentValue;
+//     }
+
+//     prevValue = currentValue;
+//   }
+
+//   return Math.abs(result);
+// };
+
 var romanToInt = function (s) {
-  const romanInteger = {
+  const ROMAN_SYMBOL_AND_VALUE = {
     I: 1,
     V: 5,
     X: 10,
@@ -13,22 +42,15 @@ var romanToInt = function (s) {
     M: 1000,
   };
 
-  let result = 0;
-  let prevValue = 0;
+  let realValue = 0;
 
-  for (let i = s.length - 1; i >= 0; i--) {
-    let currentValue = romanInteger[s[i]];
+  for (let i = 0; i < s.length; i++) {
+    let currentValue = ROMAN_SYMBOL_AND_VALUE[s[i]];
+    let nextValue = ROMAN_SYMBOL_AND_VALUE[s[i + 1]] || 0;
 
-    if (currentValue >= prevValue) {
-      result += currentValue;
-    } else {
-      result -= currentValue;
-    }
-
-    prevValue = currentValue;
+    realValue += nextValue > currentValue ? -currentValue : currentValue;
   }
-
-  return Math.abs(result);
+  return realValue;
 };
 
 console.log(romanToInt("III")); // Devrait afficher 3
