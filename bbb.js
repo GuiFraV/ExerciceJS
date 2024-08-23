e7d497837d1672a5972ef7116004e474a6cc4712
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-23 12:47:31 +0200

    Done

--- bbb.js
+++ bbb.js
@@ -23,6 +23,42 @@ const RomanToInteger = (symbol) => {
   return result;
 };
 
-console.log(RomanToInteger("III"));
-console.log(RomanToInteger("LVIII"));
-console.log(RomanToInteger("MCMXCIV"));
+// console.log(RomanToInteger("III"));
+// console.log(RomanToInteger("LVIII"));
+// console.log(RomanToInteger("MCMXCIV"));
+
+const bestTimeToBuy = (prices) => {
+  let minValue = prices[0];
+  let maxValue = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (minValue > prices[i]) {
+      minValue = prices[i];
+    } else {
+      let diff = prices[i] - minValue;
+
+      if (diff > maxValue) {
+        maxValue = diff;
+      }
+    }
+  }
+  return maxValue;
+};
+
+console.log(bestTimeToBuy([7, 1, 5, 3, 6, 4]));
+console.log(bestTimeToBuy([7, 6, 4, 3, 1]));
+
+const jumpGame = (nums) => {
+  let goal = nums.length - 1;
+
+  for (let i = nums.length; i >= 0; i--) {
+    if (nums[i] + i >= goal) {
+      goal = i;
+    }
+  }
+
+  return goal === 0;
+};
+
+console.log(jumpGame([2, 3, 1, 1, 4]));
+console.log(jumpGame([3, 2, 1, 0, 4]));
