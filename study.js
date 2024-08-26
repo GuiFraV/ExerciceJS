0a05e9433f8ec4f76411a90b689e8a4ef104d5bc
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-26 17:47:49 +0200

    STUDY DONE

--- study.js
+++ study.js
@@ -16,5 +16,20 @@ const timeToBuy = (prices) => {
   return maxProfit;
 };
 
-console.log(timeToBuy([7, 1, 5, 3, 6, 4]));
-console.log(timeToBuy([7, 6, 4, 3, 1]));
+// console.log(timeToBuy([7, 1, 5, 3, 6, 4]));
+// console.log(timeToBuy([7, 6, 4, 3, 1]));
+
+const jumpGame = (nums) => {
+  let goal = nums.length - 1;
+
+  for (let i = nums.length - 2; i >= 0; i--) {
+    if (nums[i] + i >= goal) {
+      goal = i;
+    }
+  }
+
+  return goal === 0;
+};
+
+// console.log(jumpGame([2, 3, 1, 1, 4]));
+// console.log(jumpGame([3, 2, 1, 0, 4]));
