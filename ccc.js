87fcf361c5bd7a20619481deb240fae5e206e937
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-29 09:43:39 +0200

    Done

--- ccc.js
+++ ccc.js
@@ -51,3 +51,18 @@ const timeToSell = (prices) => {
 
 // console.log(timeToSell([7, 1, 5, 3, 6, 4]));
 // console.log(timeToSell([7, 6, 4, 3, 1]));
+
+const lcp = (strs) => {
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length - 1);
+      if (prefix === "") return "";
+    }
+  }
+  return prefix;
+};
+
+console.log(lcp(["flower", "flow", "flight"]));
+console.log(lcp(["dog", "racecar", "car"]));
