5675885fcf986f183f3ca2f94987659f00e0620a
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-29 09:41:12 +0200

    Done*

--- ccc.js
+++ ccc.js
@@ -30,3 +30,24 @@ const jumpGameII = (nums) => {
   }
   return jumps;
 };
+
+const timeToSell = (prices) => {
+  let minPrices = prices[0];
+  let maxValue = 0;
+
+  for (let i = 1; i < prices.length; i++) {
+    if (minPrices > prices[i]) {
+      minPrices = prices[i];
+    } else {
+      let diff = prices[i] - minPrices;
+      if (diff > maxValue) {
+        maxValue = diff;
+      }
+    }
+  }
+
+  return maxValue;
+};
+
+// console.log(timeToSell([7, 1, 5, 3, 6, 4]));
+// console.log(timeToSell([7, 6, 4, 3, 1]));
