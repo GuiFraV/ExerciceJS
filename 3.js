2be20cd2106264ede3f6839fea367d6416e73996
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-08-30 10:05:18 +0200

    Done

--- 3.js
+++ 3.js
@@ -23,6 +23,21 @@ const RomanToInt = (s) => {
   return result;
 };
 
-console.log(RomanToInt("III"));
-console.log(RomanToInt("LVIII"));
-console.log(RomanToInt("MCMXCIV"));
+// console.log(RomanToInt("III"));
+// console.log(RomanToInt("LVIII"));
+// console.log(RomanToInt("MCMXCIV"));
+
+const lcprefix = (strs) => {
+  let firstWord = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[i].indexOf(firstWord) !== 0) {
+      firstWord = firstWord.substring(0, strs.length - 1);
+      if (firstWord === "") return "";
+    }
+  }
+  return firstWord;
+};
+
+console.log(lcprefix(["flower", "flow", "flight"]));
+console.log(lcprefix(["dog", "racecar", "car"]));
