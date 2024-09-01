57ff29a276c21a4b160aacbb0fd76bf6fbe7f55e
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-09-01 20:20:21 +0200

    Done

--- 4.js
+++ 4.js
@@ -59,5 +59,29 @@ const jG = (nums) => {
   return minJump === 0;
 };
 
-console.log(jG([2, 3, 1, 1, 4]));
-console.log(jG([3, 2, 1, 0, 4]));
+// console.log(jG([2, 3, 1, 1, 4]));
+// console.log(jG([3, 2, 1, 0, 4]));
+
+const jGII = (nums) => {
+  let minJump = 0;
+  let currentMax = 0;
+  let nextMax = 0;
+
+  for (let i = 0; i < nums.length - 1; i++) {
+    nextMax = Math.max(nextMax, i + nums[i]);
+
+    if (currentMax === i) {
+      minJump++;
+      currentMax = nextMax;
+    }
+
+    if (currentMax >= nums.length - 1) {
+      return jumps;
+    }
+  }
+
+  return minJump;
+};
+
+console.log(jGII([2, 3, 1, 1, 4]));
+console.log(jGII([2, 3, 0, 1, 4]));
