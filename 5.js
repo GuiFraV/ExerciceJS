d66202d2decdbd686247aa25b2b3382a903ca463
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-09-02 10:19:30 +0200

    Done

--- 5.js
+++ 5.js
@@ -17,5 +17,29 @@ const bestTime = (prices) => {
   return maxValue;
 };
 
-console.log(bestTime([7, 1, 5, 3, 6, 4]));
-console.log(bestTime([7, 6, 4, 3, 1]));
+// console.log(bestTime([7, 1, 5, 3, 6, 4]));
+// console.log(bestTime([7, 6, 4, 3, 1]));
+
+const jGII = (nums) => {
+  let minJump = 0;
+  let currentMax = 0;
+  let nextMax = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    nextMax = Math.max(nextMax, i + nums[i]);
+
+    if (i === currentMax) {
+      minJump++;
+      currentMax = nextMax;
+    }
+
+    if (currentMax >= nums.length - 1) {
+      return minJump;
+    }
+  }
+
+  return minJump;
+};
+
+// console.log(jGII([2, 3, 1, 1, 4]));
+// console.log(jGII([2, 3, 0, 1, 4]));
