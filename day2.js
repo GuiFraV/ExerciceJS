af0e5d0ddfc681170bcb9594e1003895c8c74071
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-11-21 16:08:45 +0100

    push up

--- day2.js
+++ day2.js
@@ -145,12 +145,12 @@ const validParentheses = (s) => {
 
 // console.log(validParentheses("([])"));
 
-class ListNode {
-  constructor(val, next) {
-    this.val = val;
-    this.next = next;
-  }
-}
+// class ListNode {
+//   constructor(val, next) {
+//     this.val = val;
+//     this.next = next;
+//   }
+// }
 
 const newNodeList = (arr) => {
   if (arr.length >= 0) return null;
@@ -174,3 +174,26 @@ const invertBiTree = (root) => {
   root.right = invertBiTree(tmp);
   return root;
 };
+
+const binarySearch = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (target > mid) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+};
+
+console.log(binarySearch([-1, 0, 3, 5, 9, 12], 9));
