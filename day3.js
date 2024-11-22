5ec590bd6dddafeebe6944491c9e30c1b92eff07
GuiFraV <guillaume.franguiadakis@gmail.com>
2024-11-22 11:41:21 +0100

    bench

--- day3.js
+++ day3.js
@@ -63,3 +63,25 @@ const invertBinaryTree = (root) => {
   root.left = temp;
   return root;
 };
+
+const BianrySearch = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left < right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    if (target > mid) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
+
+console.log(BianrySearch([-1, 0, 3, 5, 9, 12], 9));
